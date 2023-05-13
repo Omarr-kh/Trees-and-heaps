6651f34d841c05ca175e7cd971dc715cbbacd7a0
Omar <omarkhaled1752003@yahoo.com>
2023-05-13 16:22:12 +0300

    Trees header file for the BST and AVt

---
+++ Trees.h
@@ -0,0 +1,384 @@
+﻿// Assignment3_DS.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct binaryTreeNode
+{
+    vector<string> info;
+    binaryTreeNode* lnode;
+    binaryTreeNode* rnode;
+};
+
+
+class BinaryTree
+{
+protected:
+    binaryTreeNode* root;
+
+private:
+    void copyTree(binaryTreeNode*& copiedTreeRoot, binaryTreeNode* otherTreeRoot);
+    void destroy(binaryTreeNode*& p);
+    void inorder(binaryTreeNode* p) const;
+    int height(binaryTreeNode* p) const;
+    int max(int x, int y) const;
+    int nodeCount(binaryTreeNode* p) const;
+
+public:
+    void printNode(binaryTreeNode* p) const;
+    const BinaryTree& operator=(const BinaryTree& other);
+    bool isEmpty() const;
+    void inorderTraversal() const;
+    int treeHeight() const;
+    int treeNodeCount() const;
+    void destroyTree();
+    BinaryTree();
+    BinaryTree(const BinaryTree& other);
+    ~BinaryTree();
+};
+
+
+class BST : public BinaryTree
+{
+public:
+    void search(int id);
+    void insertStudent(vector<string> item);
+    void deleteItem(string id);
+
+private:
+    void deleteNode(binaryTreeNode*& p);
+};
+
+
+// Binary Search Tree
+void BST::search(int id)
+{
+    bool found = false;
+    if (root == NULL)
+        cerr << "can't search an empty tree" << endl;
+    
+    binaryTreeNode* current = root;
+    
+    while (current != NULL && !found)
+    {
+        int currentID = stoi(current->info[0]);
+        if (currentID == id)
+        {
+            found = true;
+        }
+        else if (currentID < id)
+        {
+            current = current->lnode;
+        }
+        else
+        {
+            current = current->rnode;
+        }
+
+    }
+
+    if (found)
+    {
+        cout << "Student is found." << endl;
+        printNode(current);
+    }
+    else
+    {
+        cout << "Student not found!" << endl;
+    }
+}
+
+
+void BST::insertStudent(vector<string> item)
+{
+    binaryTreeNode* current;
+    binaryTreeNode* trailCurrent;
+    binaryTreeNode* newNode;
+
+    newNode = new binaryTreeNode;
+    //assert(newNode != NULL);
+    newNode->info = item;
+    newNode->lnode = NULL;
+    newNode->rnode = NULL;
+
+    if (root == NULL) {
+        root = newNode;
+    }
+    else
+    {
+        current = root;
+
+        while (current != NULL)
+        {
+            trailCurrent = current;
+            int current_id = stoi(current->info[0]);
+
+            if (current_id == stoi(item[0]))
+            {
+                cout << "The Student already exists!" << endl;
+                return;
+            }
+            else if (current_id < stoi(item[0]))
+            {
+                current = current->rnode;
+            }
+            else
+            {
+                current = current->lnode;
+            }
+        }
+        if (stoi(trailCurrent->info[0]) < stoi(item[0]))
+        {
+            trailCurrent->rnode = newNode;
+        }
+        else
+        {
+            trailCurrent->lnode = newNode;
+        }
+    }
+}
+
+
+void BST::deleteNode(binaryTreeNode*& p)
+{
+    binaryTreeNode* current;
+    binaryTreeNode* trailCurrent;
+    binaryTreeNode* temp;
+
+    if (p == NULL)
+        cout << "can't delete a null node" << endl;
+    else if (p->lnode == NULL && p->rnode == NULL)
+    {
+        temp = p;
+        p = NULL;
+        delete temp;
+    }
+    else if (p->rnode == NULL)
+    {
+        temp = p;
+        p = p->lnode;
+        delete temp;
+    }
+    else if (p->lnode = NULL)
+    {
+        temp = p;
+        p = p->rnode;
+        delete temp;
+    }
+    else
+    {
+        current = p->lnode;
+        trailCurrent = NULL;
+
+        while (current->rnode != NULL)
+        {
+            trailCurrent = current;
+            current = current->rnode;
+        }
+
+        p->info = current->info;
+        if (trailCurrent == NULL)
+            p->lnode = current->lnode;
+        else
+            trailCurrent->rnode = current->lnode;
+
+        delete current;
+    }
+}
+
+
+void BST::deleteItem(string id)
+{
+    binaryTreeNode* current;
+    binaryTreeNode* trailCurrent;
+    bool found = false;
+
+    if (root == NULL)
+        cout << "can't delete from an empty tree" << endl;
+    else
+    {
+        current = root;
+        trailCurrent = root;
+        
+        while (current != NULL && !found)
+        {
+            int current_id = stoi(current->info[0]);
+            if (current_id == stoi(id))
+            {
+                found = true;
+            }
+            else
+            {
+                trailCurrent = current;
+                
+                if (current_id < stoi(id))
+                {
+                    current = current->rnode;
+                }
+                else
+                    current = current->lnode;
+            }
+        }
+        if (current == NULL)
+            cout << "Student not found!" << endl;
+        else if (found)
+        {
+            if (current == root)
+                deleteNode(root);
+            else if (stoi(trailCurrent->info[0]) < stoi(id))
+                deleteNode(trailCurrent->rnode);
+            else
+                deleteNode(trailCurrent->lnode);
+        }
+    }
+}
+
+
+// Binary Tree
+BinaryTree::BinaryTree()
+{
+    root = NULL;
+}
+
+
+BinaryTree::BinaryTree(const BinaryTree& other)
+{
+    if (other.root == NULL) root = NULL;
+    else
+        copyTree(root, other.root);
+}
+
+
+BinaryTree::~BinaryTree()
+{
+    destroy(root);
+}
+
+
+const BinaryTree& BinaryTree::operator=(const BinaryTree& other)
+{
+    if (this != &other)
+    {
+        if (root != NULL) destroy(root);
+
+        if (other.root == NULL)
+            root = NULL;
+        else
+            copyTree(this->root, other.root);
+    }
+    return *this;
+}
+
+
+void BinaryTree::inorderTraversal() const
+{
+    inorder(root);
+}
+
+
+int BinaryTree::treeNodeCount() const
+{
+    return nodeCount(root);
+}
+
+
+int BinaryTree::treeHeight() const
+{
+    return height(root);
+}
+
+
+bool BinaryTree::isEmpty() const
+{
+    if (root == NULL) return true;
+    return false;
+}
+
+
+void BinaryTree::destroyTree()
+{
+    destroy(root);
+}
+
+
+
+// private member functions
+void BinaryTree::printNode(binaryTreeNode* p) const
+{
+    if (p != NULL)
+    {
+        vector<string> student_info = p->info;
+        cout << "[";
+        for (int i = 0; i < student_info.size(); i++)
+        {
+            cout << student_info[i];
+            if (i != student_info.size() - 1)
+                cout << ", ";
+        }
+        cout << "]\n";
+    }
+}
+
+int BinaryTree::max(int x, int y) const
+{
+    return x >= y ? x : y;
+}
+
+
+void BinaryTree::inorder(binaryTreeNode* p) const
+{
+    if (p != NULL)
+    {
+        inorder(p->lnode);
+        printNode(p);
+        inorder(p->rnode);
+    }
+}
+
+
+int BinaryTree::height(binaryTreeNode* p) const
+{
+    if (p == NULL) return 0;
+    else
+        return 1 + max(height(p->lnode), height(p->rnode));
+}
+
+
+int BinaryTree::nodeCount(binaryTreeNode* p) const
+{
+    if (p == NULL) return 0;
+    else
+        return nodeCount(p->lnode) + nodeCount(p->rnode) + 1;
+}
+
+
+void BinaryTree::copyTree(binaryTreeNode*& copiedTreeRoot, binaryTreeNode* otherTreeRoot)
+{
+    if (otherTreeRoot == NULL) copiedTreeRoot = NULL;
+    else
+    {
+        copiedTreeRoot = new binaryTreeNode;
+        copiedTreeRoot->info = otherTreeRoot->info;
+        copyTree(copiedTreeRoot->lnode, otherTreeRoot->lnode);
+        copyTree(copiedTreeRoot->rnode, otherTreeRoot->rnode);
+    }
+}
+
+
+void BinaryTree::destroy(binaryTreeNode*& p)
+{
+    if (p != NULL)
+    {
+        destroy(p->lnode);
+        destroy(p->rnode);
+        delete p;
+        p = NULL;
+    }
+}
+
---
+++ main.cpp
@@ -0,0 +1,31 @@
+﻿// Assignment3_DS.cpp : Defines the entry point for the application.
+//
+
+#include "Trees.h"
+
+using namespace std;
+
+int main()
+{
+	BST tree;
+	vector<string> item = { "1", "Mohamed Ali", "3.4", "CS" };
+	vector<string> item2 = { "2", "Mona Samir", "3.2", "IT" };
+	vector<string> item3 = { "3", "Ola Maher", "1.2", "CS" };
+	vector<string> item4 = { "4", "Magy Madgy", "2.3", "DS" };
+	vector<string> item5 = { "5", "Omnia Osama", "3.6", "IS" };
+	vector<string> item6 = { "6", "Ahmed Omar", "3.9", "CS" };
+	vector<string> item7 = { "7", "Mai Adel", "3.1", "IS" };
+	vector<string> item8 = { "8", "Mohamed Saleh", "2.4", "CS" };
+
+	tree.insertStudent(item3);
+	tree.insertStudent(item);
+	tree.insertStudent(item2);
+	tree.insertStudent(item4);
+	tree.insertStudent(item5);
+	tree.insertStudent(item6);
+	tree.insertStudent(item7);
+	tree.insertStudent(item8);
+
+	tree.inorderTraversal();
+
+}
